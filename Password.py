c61bc84a62652910eef21d5a06693e0aee8146e7
RandomOtakuCoder <82604939+RandomOtakuCoder@users.noreply.github.com>
2021-04-22 15:08:27 +0800

    Add files via upload

---
+++ Password.py
@@ -0,0 +1,21 @@
+p = "j*L3f&"
+c = 5
+print("*You only have 5 chances")
+a = input("Please enter password:")
+if a != p:
+    c = c - 1
+    print("Password incorrect, you still have", c, "chances left")
+while a != p:
+    c = c - 1
+    a = input("Please re-enter password:")
+    if c > 1:
+        print("Password incorrect, you still have", c, "chances left")
+    if c == 1:
+        print("Password incorrect, you still have", c, "chance left")
+    if c == 0:
+        print(
+            "Account locked due to mutiple unsucessful attempt, please contect support for more infomation."
+        )
+        break
+if a == p:
+    print("Login sucessful!")
